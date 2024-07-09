efb1793b3bf079ec349e100ec262ae853b18f179
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-07-09 10:21:02 +0200

    feat: docker configuration

--- html/index.php
+++ html/index.php
@@ -1,3 +1,11 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Info</title>
+</head>
+<body>
+<h1>PHP Info</h1>
+<?php phpinfo(); ?>
+</body>
+</html>
