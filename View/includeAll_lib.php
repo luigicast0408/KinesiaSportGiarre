22f7226ec5eeaec82115e311a8e056fc55e1f718
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-08-30 21:10:37 +0200

    feat(includeAll_lib.php): add function for include all bootstrap lib

---
+++ View/includeAll_lib.php
@@ -0,0 +1,14 @@
+<?php
+function includeStyles(): void
+{
+    echo '
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    ';
+}
+
