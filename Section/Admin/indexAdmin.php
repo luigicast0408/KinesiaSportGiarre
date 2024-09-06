d3925fafdae23cc1a4daf45cbaa74899faa35f3f
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-09-06 18:14:53 +0200

    feat(indexAdmin.php) add home page for admin

---
+++ Section/Admin/indexAdmin.php
@@ -0,0 +1,23 @@
+<?php
+require_once ("../../View/navbarAdmin.php");
+require_once ("../../View/includeAll_lib.php");
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../style/style_footer.css">
+    <link rel="stylesheet" type="text/css" href="../../style/style_nav.css">
+    <link rel="stylesheet" type="text/css" href="../../style/style_cards.css">
+    <link rel="stylesheet" type="text/css" href="../../style/style_header.css">
+    <title>ADMIN </title>
+    <?php includeStyles() ?>
+</head>
+<body>
+<?php renderAdminNavbar() ?>
+
+</body>
+</html>
+
+
