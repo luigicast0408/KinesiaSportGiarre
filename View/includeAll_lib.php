03560584fc041ab682441fd2296effb39d9d1859
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-09-08 20:42:16 +0200

    fix(navbar.php): include some script for carousel in gallery section

--- View/includeAll_lib.php
+++ View/includeAll_lib.php
@@ -6,5 +6,8 @@ function includeStyles(): void
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 ';
 }
