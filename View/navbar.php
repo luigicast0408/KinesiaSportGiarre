5e12c6939f80a308e2333232994ad37031acf19a
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-09-17 12:23:27 +0200

    fix(navbar.php): fix the image logo size

--- View/navbar.php
+++ View/navbar.php
@@ -7,7 +7,7 @@ function navbar(): void
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="d-flex align-items-center w-100">
                 <a class="navbar-brand d-flex align-items-center" href="/Section/Home/index.php">
-                    <img src="/Images/logo.png" id="l1" alt="Logo">
+                    <img src="/Images/logo.png" id="l1" alt="Logo" style="height: auto; width: 20%"> 
                     <span style="font-size: 2rem; font-weight: bold; margin-left: 10px;">ASD Kinesia</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -89,7 +89,7 @@ function navbar(): void
                             <a class="nav-link" href="/Section/TrainingPlans/indexTrainingPlans.php">Schede Allenamento</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/Section/Reviews/indexReviews.php">Recenzioni</a>
+                            <a class="nav-link" href="/Section/Reviews/addReviews/indexReviews.php">Recenzioni</a>
                         </li> 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">';
