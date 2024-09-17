54f6b71ab93a069532272cb8dff6b480a6e7e07f
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-09-17 16:38:24 +0200

    fix(contact us section): improve the GUI

--- Section/ContactUs/indexContactUs.php
+++ Section/ContactUs/indexContactUs.php
@@ -9,29 +9,12 @@ require_once("../../View/includeAll_lib.php");
 <head>
     <meta charset="UTF-8">
     <title>Contatti</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../style/style_footer.css">
     <link rel="stylesheet" type="text/css" href="../../style/style_nav.css">
     <link rel="stylesheet" type="text/css" href="../../style/style_header.css">
     <link rel="stylesheet" type="text/css" href="../../style/style_cards.css">
     <?php includeStyles() ?>
-    <style>
-        .contact-info {
-            margin-top: 20px;
-        }
-        .map-container {
-            margin-top: 20px;
-        }
-        .form-control {
-            border-radius: 20px;
-        }
-        .btn-custom {
-            border-radius: 20px;
-            width: 100%;
-        }
-        .card {
-            background-color: #e3f2fd; /* Light blue background color */
-        }
-    </style>
 </head>
 <body>
 <?php navbar(); ?>
@@ -42,49 +25,77 @@ require_once("../../View/includeAll_lib.php");
 
 <div class="container-fluid">
     <div class="row" style="padding-top: 2%">
-        <div class="col-md-6">
+        <div class="col-lg-6 col-md-12">
             <div class="card shadow-2-strong card-registration" style="border-radius: 25px">
                 <div class="card-title">
                     <h3>Contattaci</h3>
                 </div>
                 <div class="card-body">
-                    <form action="#" method="POST"> <!--  todo: add php file (send mail) -->
+                    <form action="#" method="POST" id="contactForm"> <!--  todo: add php file (send mail) -->
                         <div class="form-group">
                             <label for="name">Nome</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il tuo nome" required aria-label="Nome">
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Inserisci la tua email" required aria-label="Email">
                         </div>
                         <div class="form-group">
                             <label for="subject">Oggetto</label>
-                            <input type="text" class="form-control" id="subject" name="subject" required>
+                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Oggetto del messaggio" required aria-label="Oggetto">
                         </div>
                         <div class="form-group">
                             <label for="message">Messaggio</label>
-                            <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+                            <textarea class="form-control" id="message" name="message" rows="4" placeholder="Scrivi il tuo messaggio" required aria-label="Messaggio"></textarea>
                         </div>
-
                         <button type="submit" class="btn btn-primary btn-custom" style="margin-top: 3%">Invia</button>
                     </form>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-6 contact-info">
+        <div class="col-lg-6 col-md-12 contact-info">
             <h4>Informazioni di Contatto</h4>
-            <p><strong>Telefono:</strong> +39 123 456 7890</p>
-            <p><strong>Email:</strong> info@example.com</p>
-            <p><strong>Indirizzo:</strong> Via Roma, 123, 00100 Roma, Italia</p>
+            <ul class="list-unstyled">
+                <li>
+                    <p><i class="fas fa-phone" style="color: black;"></i> <strong>Telefono:</strong>
+                        <a href="tel:+393477157985" class="footer-link">3477157985 (Vera Sorbello)</a>
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fas fa-phone" style="color: black;"></i> <strong>Telefono:</strong>
+                        <a href="tel:+393280850471" class="footer-link">3280850471 (Salvatore Parisi)</a>
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fas fa-envelope" style="color: black;"></i> <strong>Email:</strong>
+                        <a href="mailto:info@example.com" class="footer-link">info@example.com</a>
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fas fa-map-marker-alt" style="color: black;"></i> <strong>Indirizzo:</strong>
+                        <span>Via Rosolino Pilo, 32, 95014 Giarre CT</span>
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fas fa-map-marker-alt" style="color: black;"></i> <strong>Indirizzo:</strong>
+                        <span>Via Vincenzo Bellini, 11, 95014 Giarre CT</span>
+                    </p>
+                </li>
+            </ul>
         </div>
     </div>
 
-    <div class="map-container">
-        <h4>La Nostra Posizione</h4>
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3121.5895390288697!2d12.496365115396109!3d41.90278317936548!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f60674d7e8c6b%3A0xa54e5a3eec44c1ed!2sVia%20Roma%2C%20123%2C%2000100%20Roma%20RM%2C%20Italy!5e0!3m2!1sen!2sus!4v1633970883492!5m2!1sen!2sus"
-            width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <div class="map-container" style="padding-top: 5%">
+        <h4>Le Nostre sedi</h4>
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1834.6322487519196!2d15.19025349839478!3d37.72790080000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1314070d0172c513%3A0x26caa15e0f13133c!2sASD%20Kinesia!5e1!3m2!1sit!2sit!4v1726581960268!5m2!1sit!2sit" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" aria-label="Mappa Sede ASD Kinesia"></iframe>
+            </div>
+            <div class="col-lg-6 col-md-12 mb-3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1834.6484254496218!2d15.18715429839477!3d37.72724780000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131407b44de16af7%3A0x5f23b49236164bde!2sKinesia%20Sport%20%26%20Fighting!5e1!3m2!1sit!2sit!4v1726582007255!5m2!1sit!2sit" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" aria-label="Mappa Sede Kinesia Sport & Fighting"></iframe>
+            </div>
+        </div>
     </div>
 </div>
 
