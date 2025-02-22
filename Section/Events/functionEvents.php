e501179ab9ad64a63216eee87f47b87e4f06490f
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-02-22 12:11:20 +0100

    fix(functionEvents.php): pretty code

--- Section/Events/functionEvents.php
+++ Section/Events/functionEvents.php
@@ -5,42 +5,40 @@ require_once("functionEvents.php");
 function showEvents(): void
 {
     $connection = DBConnect();
-    $query = "SELECT *  FROM Events ";
+    $query = "SELECT * FROM Events WHERE  type='saggio'";
     $stm = $connection->prepare($query);
     $stm->execute();
     $events = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-    $html = '<div class="container-fluid"> 
+    $html = '<div class="container py-4"> 
     <div class="row">';
-    $i = 0;
 
-    foreach ($events as $row)
-    {
-        $class = "card-bg-" . ($i);
+    foreach ($events as $index => $row) {
         $html .= '
-        <div class="col-md-4 mb-4">
-            <div class="card h-100 '.$class.'">
-                <img class="card-img-top" src="' . $row['image'] . '" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">' . $row['event_name'] . '</h4>
-                    <p class="card-text">' . $row['type'] . '</p>
-                    <p class="card-text">' . $row['event_description'] . '</p>
-                    <p class="card-text">' . $row['date'] . '</p>
+    <div class="col-md-4 mb-4 d-flex">
+        <div class="card shadow-sm border-light rounded-lg overflow-hidden d-flex flex-column w-100">
+            <div class="position-relative">
+                <img class="card-img-top event-image" src="' . $row['image_link'] . ' " 
+                     alt="' . htmlspecialchars($row['event_name']) . '" 
+                     style="height: 1020px; object-fit: cover; width: 100%; transition: transform 0.3s ease-in-out;">
+                <div class="event-date position-absolute top-0 end-0 bg-primary text-white px-3 py-1 rounded-bottom-start">
+                    <small>' . date("d M Y", strtotime($row['date'])) . '</small>
                 </div>
             </div>
+            <div class="card-body d-flex flex-column text-center flex-grow-1">
+                <h5 class="card-title text-dark fw-bold">' . htmlspecialchars($row['event_name']) . '</h5>
+                <p class="card-text text-muted flex-grow-1">' . nl2br(htmlspecialchars($row['event_description'])) . '</p>
+            </div>
         </div>
-        ';
-
-        $i++;
-        if ($i % 3 == 0) {
-            $html .= '</div><div class="row">';
-        }
+    </div>';
     }
+
+
+
+
+
     $html .= '</div></div>';
 
     echo $html;
-
     $connection = null;
 }
-
-?>
